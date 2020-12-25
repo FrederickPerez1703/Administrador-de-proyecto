5dc39b81b52d4170be0f4df7307bee681caad00b
Frederick Perez <frederickperezdelacruz@outlook.com>
2020-12-25 09:57:44 +0800

    Inicio del proyecto v1

---
+++ AdministradordeProyecto/src/main/java/com/proyecto/AdministradordeProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministradordeProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministradordeProyectoApplication.class, args);
+	}
+
+}
---
+++ AdministradordeProyecto/src/test/java/com/proyecto/AdministradordeProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradordeProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
